2bbbeee2b4809db9128236ee7243719eff9c0190
rohitsharma3030 <156637660+rohitsharma3030@users.noreply.github.com>
2025-05-09 10:42:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+# Update & install dependencies for Chromium
+apt-get update
+apt-get install -y wget ca-certificates fonts-liberation libappindicator3-1 libasound2 \
+libatk-bridge2.0-0 libnspr4 libnss3 libxss1 xdg-utils
+
+# Start Node app
 node index.js
